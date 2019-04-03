cca280733aee17c24b459f66e1acd3f61be5d81b
jamesm2w <jamesmacerwright@gmail.com>
2019-04-03 08:56:08 +0100

    Added sponsor banner

--- js/init.js
+++ js/init.js
@@ -4,6 +4,17 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $(".scrollspy").scrollSpy();
+    $('.carousel').carousel({
+      numVisible: 8,
+      dist: 0
+    });
+
+    setInterval(() => {
+      let elem = document.getElementById("sponsor-carousel");
+
+      var instance = M.Carousel.getInstance(elem);
+      instance.next();
+    }, 2500);
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
